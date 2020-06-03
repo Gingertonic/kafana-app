475d4723cb6cea97c4186cd2a8b729b29535b8f5
Gingertonic <bethmschofield@gmail.com>
2020-06-03 15:02:09 +0100

    basic socket server setup

--- chat-server/server.js
+++ chat-server/server.js
@@ -3,6 +3,12 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 
+const io = require("socket.io")(server) // integrate our http server with a new instance of socket.io
+
+io.on('connection', () => {
+    console.log('A new connection has been made!')
+})
+
 app.get('/', (req, res) => {
     res.send('Hi there')
 });
